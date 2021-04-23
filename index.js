ea3fb9df1c3804d29efa88a7ba96c2125f66a375
jfoclpf <joao.pimentel.ferreira@gmail.com>
2021-04-24 00:35:22 +0200

    update

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const path = require('path')
 const shapefile = require('shapefile')
+const async = require('async')
 
-shapefile.open(path.join('res','concelhos.shp'))
-  .then(source => source.read()
-    .then(function log(result) {
-      if (result.done) return;
-      console.log(result.value);
-      return source.read().then(log);
-    }))
-  .catch(error => console.error(error.stack))
+const shapeFiles = {
+  freguesias: 'Cont_AAD_CAOP2017',
+  concelhos: 'concelhos',
+  distritos: 'distritos'
+}
+
+const geojson = {}
+
+async.forEachOf(shapeFiles, (value, key, callback) => {
+  shapefile.read(
+    path.join('res', value + '.shp'),
+    path.join('res', value + '.dbf'),
+    { encoding: 'utf-8' }
+  ).then(geojsonLoc => {
+    geojson[key] = geojsonLoc
+    callback()
+  }).catch((error) => {
+    console.error('Error reading freguesias')
+    callback(error.stack)
+  })
+}, err => {
+  if (err) {
+    console.error(err.message)
+    process.exitCode = 1
+  } else {
+    console.log(geojson)
+  }
+})
