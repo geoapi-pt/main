19b839d3d309a871f86e91d086b64a3fb46b378f
jfoclpf <joao.pimentel.ferreira@gmail.com>
2021-04-24 22:09:26 +0200

    update

--- index.js
+++ index.js
@@ -1,39 +1,26 @@
 const path = require('path')
 const shapefile = require('shapefile')
 const PolygonLookup = require('polygon-lookup')
-const async = require('async')
 
-const shapeFiles = {
-  freguesias: 'Cont_AAD_CAOP2017',
-  concelhos: 'concelhos',
-  distritos: 'distritos'
-}
-
-const geojson = {}
-
-async.forEachOf(shapeFiles, (value, key, callback) => {
-  shapefile.read(
-    path.join('res', value + '.shp'),
-    path.join('res', value + '.dbf'),
-    { encoding: 'utf-8' }
-  ).then(geojsonLoc => {
-    geojson[key] = geojsonLoc
-    callback()
-  }).catch((error) => {
-    console.error('Error reading freguesias')
-    callback(error.stack)
-  })
-}, err => {
-  if (err) {
-    console.error(err.message)
-    process.exitCode = 1
-  } else {
-    const point = {
-      lat: 38.569566888238924,
-      lon: -7.905490149218761
-    }
-    const lookupDistritos = new PolygonLookup(geojson.distritos)
-    const distrito = lookupDistritos.search(point.lon, point.lat)
-    console.log(distrito.properties.NAME_1)
+shapefile.read(
+  path.join('res', 'Cont_Troco_CAOP2020.shp'),
+  path.join('res', 'Cont_Troco_CAOP2020.dbf'),
+  { encoding: 'utf-8' }
+).then(geojson => {
+  const point = {
+    lat: 0, // 39.93431895133549,
+    lon: 0 // -8.545575208651075
   }
+  console.log(geojson.features[0])
+  /* for (const key in geojson.features) {
+    console.log(geojson.features[key].properties.Concelho)
+  } */
+  const lookupFreguesias = new PolygonLookup(geojson)
+  const freguesia = lookupFreguesias.search(point.lon, point.lat)
+  console.log(freguesia)
+  // console.log(freguesia.geometry.coordinates)
+}).catch((error) => {
+  console.error('Error reading freguesias')
+  console.error(error.stack)
+  process.exitCode = 1
 })
