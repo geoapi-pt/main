10bf32e259c06a4cb1f832484a3c4977bac240f6
jfoclpf <joao.pimentel.ferreira@gmail.com>
2021-10-10 15:08:01 +0200

    just comments

--- prepareServer.js
+++ prepareServer.js
@@ -412,12 +412,12 @@ function extractParishInfoFromStr (str) {
 function normalizeName (name) {
   if (typeof name === 'string') {
     return name
-      .trim()
+      .trim() // removes leading and trailing spaces
       .toLowerCase()
       .replace(/\.(\w)/g, '. $1') // add space after a dot followed by letter: 'N.AB' -> 'N. AB'
       .replace(/\s(dos|das|de|da)\s/g, ' ') // remove words 'dos', 'das', 'de' e 'da'
       .replace(/\s+/g, ' ') // removes excess of whitespaces
-      .normalize('NFD').replace(/[\u0300-\u036f]/g, '') // removes diacritics
+      .normalize('NFD').replace(/[\u0300-\u036f]/g, '') // removes diacritics 'á'->'a', 'ç'->'c'
   } else {
     return null
   }
