d6a8d84fa4a4f1ab9225268807768662a44638d1
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2022-07-30 21:48:42 +0200

    introdução de códigos postais no index.html

---
+++ js/index.html/getPostalCodesInfo.js
@@ -0,0 +1,56 @@
+/* global fetch */
+
+const geoApiUrl = 'https://geoapi.pt'
+
+const inputCodigoPostal = document.getElementById('codigo-postal')
+
+inputCodigoPostal.addEventListener('input', () => {
+  if (!isPostalCodeOK(inputCodigoPostal.value)) {
+    inputCodigoPostal.style.borderColor = 'red 1px solid'
+  } else {
+    inputCodigoPostal.style.borderColor = ''
+    fetch(`${geoApiUrl}/cp/${inputCodigoPostal.value}?json=1`)
+      .then(res => res.json())
+      .then((cpResults) => {
+        let html = ''
+
+        if (Array.isArray(cpResults)) {
+          cpResults.forEach((res, idx, array) => {
+            for (const el in res) {
+              html += `<tr><th>${el}</th><td>${res[el]}</td></tr>`
+            }
+            if (idx !== array.length - 1) {
+              html += '<tr><th>-----</th><td>-----</td></tr>'
+            }
+          })
+        } else {
+          for (const el in cpResults) {
+            html += `<tr><th>${el}</th><td>${cpResults[el]}</td></tr>`
+          }
+        }
+
+        document.getElementById('result').innerHTML = html
+      })
+      .catch((err) => {
+        console.error('error fetching códigos postais', err)
+      })
+  }
+})
+
+function isPostalCodeOK (postalCodeStr_) {
+  let postalCodeStr = postalCodeStr_.trim()
+
+  if (postalCodeStr.length !== 8) {
+    return false
+  }
+
+  postalCodeStr = postalCodeStr.replace(/\u2013|\u2014/g, '-') // it replaces all &ndash; (–) and &mdash; (—) symbols with simple dashes (-)
+
+  // regex format for 0000-000 or 0000 000
+  // http://stackoverflow.com/questions/2577236/regex-for-zip-code
+  if (postalCodeStr.match(/^\d{4}(?:[-\s]\d{3})?$/)) {
+    return true
+  } else {
+    return false
+  }
+}
