79902f3060ee86287681c1bacd30ba2353f98717
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2022-09-05 18:08:42 +0200

    move shieldsio to separate module

---
+++ js/server-modules/shieldsioCounters.js
@@ -0,0 +1,49 @@
+
+/* requests counter exposed as shieldsIO JSON endpoint https://shields.io/endpoint */
+
+module.exports = { setTimers, incrementCounters, loadExpressRoutes }
+
+// counter of requests per hour
+let requestsCounterPerHour = 0
+let requestsLastHour = 0
+
+// counter of requests per day
+let requestsCounterPerDay = 0
+let requestsLastDay = 0
+
+function setTimers () {
+  setInterval(() => {
+    requestsLastHour = requestsCounterPerHour
+    requestsCounterPerHour = 0
+  }, 1000 * 60 * 60)
+
+  setInterval(() => {
+    requestsLastDay = requestsCounterPerDay
+    requestsCounterPerDay = 0
+  }, 1000 * 60 * 60 * 24)
+}
+
+function incrementCounters () {
+  requestsCounterPerHour++
+  requestsCounterPerDay++
+}
+
+function loadExpressRoutes (app) {
+  app.get('/shieldsio/requestsLastHour', function (req, res) {
+    res.json({
+      schemaVersion: 1,
+      label: 'Requests on last hour',
+      message: requestsLastHour.toString(),
+      color: 'orange'
+    })
+  })
+
+  app.get('/shieldsio/requestsLastDay', function (req, res) {
+    res.json({
+      schemaVersion: 1,
+      label: 'Requests on last day',
+      message: requestsLastDay.toString(),
+      color: 'orange'
+    })
+  })
+}
