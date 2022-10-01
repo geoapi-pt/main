e11de692483d8e3ce7064a17ba1322577ea0b37e
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2022-10-01 22:29:17 +0200

    create configs file and optimize handlebars

--- src/server/services/consoleApiStartupInfo.js
+++ src/server/services/consoleApiStartupInfo.js
@@ -1,6 +1,6 @@
 const colors = require('colors/safe')
 
-module.exports = ({ serverPort, mainPageUrl }) => {
+module.exports = ({ serverPort, gitProjectUrl }) => {
   console.log('Listening on port ' + serverPort)
   console.log('To stop server press ' + colors.red.bold('CTRL+C') + '\n')
   console.log('*******************************************************************************')
@@ -13,6 +13,6 @@ module.exports = ({ serverPort, mainPageUrl }) => {
   console.log(`**${Array(6).join(' ')}${colors.green.bold('http://localhost:' + serverPort + '/cp/2495-300')}${Array(38).join(' ')}**`)
   console.log(`**${Array(6).join(' ')}${colors.green.bold('http://localhost:' + serverPort + '/cp/1950')}${Array(42).join(' ')}**`)
   console.log('**                                                                           **')
-  console.log(`**          for instructions see ${colors.cyan.bold(mainPageUrl)}${Array(23).join(' ')}**`)
+  console.log(`**          for instructions see ${colors.cyan.bold(gitProjectUrl)}${Array(23).join(' ')}**`)
   console.log('*******************************************************************************')
 }
