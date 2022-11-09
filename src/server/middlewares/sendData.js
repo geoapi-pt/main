f486536df0cf8639824d52afaedd8ec0df452366
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2022-11-09 22:30:53 +0100

    bug solved

--- src/server/middlewares/sendData.js
+++ src/server/middlewares/sendData.js
@@ -12,7 +12,7 @@ module.exports = ({ defaultOrigin, gitProjectUrl, mainTitle, siteDescription, sh
       res.set('Connection', 'close')
       if (
         req.accepts(['html', 'json']) === 'json' ||
-        req.get('accept').includes('application/json') ||
+        (req.get('accept') && req.get('accept').includes('application/json')) ||
         parseInt(req.query.json) || req.query.json === 'true'
       ) {
         res.json(dataToBeSent)
