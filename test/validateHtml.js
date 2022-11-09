1654ee683503aa840a9de84d240980bc48c73c36
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2022-11-09 22:42:02 +0100

    implement html validation

---
+++ test/validateHtml.js
@@ -0,0 +1,123 @@
+/*
+  script that runs a http server on localhost and then html-validates
+  using a LOCAL html validator for all the html pages served
+*/
+
+console.log('Validating html/hbs pages using local html validator (html-validate)...')
+
+const TEST_PORT = 8082
+
+const path = require('path')
+const async = require('async')
+const got = require('got')
+const colors = require('colors')
+const util = require('util')
+const ProgressBar = require('progress')
+const debug = require('debug')('test:validateHtml')
+
+const HTMLValidate = require('html-validate').HtmlValidate
+const htmlvalidate = new HTMLValidate({
+  extends: ['html-validate:recommended'],
+  rules: {
+    'no-trailing-whitespace': 'off',
+    'attr-case': 'off',
+    'long-title': 'off',
+    'wcag/h30': 'off',
+    'prefer-tbody': 'off',
+    'wcag/h32': 'off',
+    'prefer-button': 'off',
+    'prefer-native-element': 'off',
+    'no-autoplay': 'off'
+  }
+})
+
+const testServer = require(path.join(__dirname, 'serverForTests'))
+
+// Array of url paths whose html is to validate
+const pathnamesToValidateArr = require(path.join(__dirname, 'openApiPaths'))()
+
+const bar = new ProgressBar('[:bar] :percent :info', { total: pathnamesToValidateArr.length + 1, width: 80 })
+
+async.series([startsHttpServer, validateHtmlOnAllPages],
+  // done after execution of above funcitons
+  function (err, results) {
+    testServer.closeServer()
+
+    bar.tick({ info: '' })
+    bar.terminate()
+
+    if (err) {
+      console.error(Error(err))
+      process.exitCode = 1
+    } else {
+      console.log(colors.green('All html/hbs pages validated OK\n'))
+      process.exitCode = 0
+    }
+  }
+)
+
+// starts http server on localhost on test default port
+function startsHttpServer (callback) {
+  console.time('timeToTestServer')
+  console.log('Please wait for server to start on...')
+
+  testServer.startsServerForTests(
+    ['--port', TEST_PORT],
+    function () {
+      console.log('server started')
+      callback()
+    }, function (err) {
+      console.error(err)
+      callback(Error(err))
+    })
+}
+
+// validates html code of pages using validator.w3.org/nu
+function validateHtmlOnAllPages (next) {
+  async.eachOfSeries(pathnamesToValidateArr, validatePage, function (err) {
+    if (err) {
+      next(Error('\nError validating html on pages: ' + err.message))
+    } else {
+      debug('All html pages validated')
+      next()
+    }
+  })
+}
+
+function validatePage (pathname, key, callback) {
+  const url = encodeURI(`http://localhost:${TEST_PORT}${pathname}`)
+  got(url)
+    .text()
+    .then(body => {
+      if (!body) {
+        callback(Error('error on url: ' + url))
+        return
+      }
+
+      const report = htmlvalidate.validateString(body)
+      if (!report.valid) {
+        console.error('\n\nERROR COUNT: ', report.results[0].errorCount)
+        console.error(util.inspect(report.results[0].messages, false, null, true /* enable colors */))
+        console.error(addLinesToStr(body))
+        callback(Error(('Package html-validate found ' + report.results[0].errorCount + ' HTML errors on ' + pathname).error))
+      } else {
+        debug(pathname)
+        bar.tick({ info: pathname })
+        callback()
+      }
+    })
+    .catch(err => {
+      callback(Error(err.message + ', error on url: ' + url))
+    })
+}
+
+// for debug purposes. On a big string of code with many breaklines,
+// adds after a breakline, the correspondig line number
+// from "abc\ndef\nghi" => "1: abc\n 2: def\n 3: ghi"
+function addLinesToStr (str) {
+  const arr = str.split('\n')
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = (i + 1).toString().padStart(4, ' ') + ':  ' + arr[i] + '\n'
+  }
+  return arr.join('')
+}
