5f0ed8e1b70c96e5ace461aab22a1941d74305cf
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2022-12-04 14:45:36 +0100

    a file is needed to store counters after server restart

--- src/server/services/shieldsioCounters.js
+++ src/server/services/shieldsioCounters.js
@@ -1,5 +1,10 @@
-
 /* requests counter exposed as shieldsIO JSON endpoint https://shields.io/endpoint */
+
+const fs = require('fs')
+const path = require('path')
+
+// a file is needed to store counters after server restart
+const countersFile = path.join(__dirname, 'shieldsioCounters.json')
 
 module.exports = { setTimers, incrementCounters, loadExpressRoutes }
 
@@ -11,14 +16,25 @@ let requestsLastHour = 0
 let requestsCounterPerDay = 0
 let requestsLastDay = 0
 
+try {
+  const countersStateOnFile = JSON.parse(fs.readFileSync(countersFile))
+  requestsLastHour = countersStateOnFile.requestsLastHour
+  requestsLastDay = countersStateOnFile.requestsLastDay
+} catch {
+  requestsLastHour = 0
+  requestsLastDay = 0
+}
+
 function setTimers () {
   setInterval(() => {
     requestsLastHour = requestsCounterPerHour
+    updateCountersOnFile()
     requestsCounterPerHour = 0
   }, 1000 * 60 * 60)
 
   setInterval(() => {
     requestsLastDay = requestsCounterPerDay
+    updateCountersOnFile()
     requestsCounterPerDay = 0
   }, 1000 * 60 * 60 * 24)
 }
@@ -46,4 +62,10 @@ function loadExpressRoutes (app) {
       color: 'orange'
     })
   })
+}
+
+// a file is needed to store counters after server restart
+function updateCountersOnFile () {
+  const counters = { requestsLastHour, requestsLastDay }
+  fs.writeFile(countersFile, JSON.stringify(counters))
 }
