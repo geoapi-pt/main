f6a6cecbf4e021a62c7187a6aac19810459c80da
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2023-02-27 11:55:37 +0100

    create script to generate geojsons of districts

---
+++ routines/generateGeojson/generateDistricts.js
@@ -0,0 +1,101 @@
+/* From Carta Administrativa with all GeoJSON files of parishes,
+   generates GeoJSON files of municipalities */
+
+const fs = require('fs')
+const path = require('path')
+const turf = require('@turf/turf')
+const ProgressBar = require('progress')
+const appRoot = require('app-root-path')
+
+const getGeojsonRegions = require(path.join(appRoot.path, 'src', 'server', 'services', 'getGeojsonRegions.js'))
+
+const districtsGeojsonDir = path.join(appRoot.path, 'res', 'geojson', 'districts')
+
+let regions
+
+// geojson data for districts (aglomerated from parishes geojson data)
+// each key is a district INE code
+const geojsonDistricts = {}
+
+getGeojsonRegions((err, _regions) => {
+  if (!err) {
+    regions = _regions
+    generategeojsonDistricts()
+    saveFiles()
+    console.log(`GeoJSON files generated OK into ${path.relative(appRoot.path, districtsGeojsonDir)}`)
+  }
+})
+
+function generategeojsonDistricts () {
+  let bar = new ProgressBar(
+    'Linking parishes to districts :percent', { total: Object.keys(regions).length }
+  )
+
+  // join parishes that correspond to the same district
+  for (const key in regions) {
+    regions[key].geojson.features.forEach(parish => {
+      const codigoine = parish.properties.DICOFRE || parish.properties.Dicofre
+      if (codigoine.length !== 6) {
+        console.error(parish)
+        throw Error('Codigo INE for parish must have 6 digits')
+      } else {
+        // the district code are the first 2 digits out of 6 digits
+        const districtCode = codigoine.slice(0, 2)
+
+        if (geojsonDistricts.hasOwnProperty(districtCode)) {  // eslint-disable-line
+          geojsonDistricts[districtCode].freguesias.push(cloneObj(parish))
+        } else {
+          geojsonDistricts[districtCode] = {}
+          geojsonDistricts[districtCode].freguesias = [cloneObj(parish)]
+        }
+      }
+    })
+    bar.tick()
+  }
+
+  bar = new ProgressBar(
+    'Merging parishes into districts :percent', { total: Object.keys(geojsonDistricts).length }
+  )
+
+  // now merge parishes geojson for each district and compute bbox and centers
+  for (const key in geojsonDistricts) {
+    if (geojsonDistricts[key].freguesias.length >= 2) {
+      geojsonDistricts[key].municipio =
+        geojsonDistricts[key].freguesias.reduce(
+          (accumulator, currentValue) => turf.union(accumulator, currentValue),
+          geojsonDistricts[key].freguesias[0]
+        )
+    } else if (geojsonDistricts[key].freguesias.length === 1) {
+      geojsonDistricts[key].municipio = cloneObj(geojsonDistricts[key].freguesias[0])
+    } else {
+      console.error(geojsonDistricts[key])
+      throw Error('wrong length')
+    }
+
+    geojsonDistricts[key].municipio.properties = { Dicofre: key }
+    geojsonDistricts[key].municipio.bbox = turf.bbox(geojsonDistricts[key].municipio)
+
+    const centros = {}
+    centros.centro = turf.center(geojsonDistricts[key].municipio).geometry.coordinates
+    centros.centroide = turf.centroid(geojsonDistricts[key].municipio).geometry.coordinates
+    centros.centroDeMassa = turf.centerOfMass(geojsonDistricts[key].municipio).geometry.coordinates
+    centros.centroMedio = turf.centerMean(geojsonDistricts[key].municipio).geometry.coordinates
+    centros.centroMediano = turf.centerMedian(geojsonDistricts[key].municipio).geometry.coordinates
+    geojsonDistricts[key].municipio.properties.centros = centros
+
+    bar.tick()
+  }
+}
+
+function saveFiles () {
+  for (const key in geojsonDistricts) {
+    fs.writeFileSync(
+      path.join(districtsGeojsonDir, key + '.json'),
+      JSON.stringify(geojsonDistricts[key])
+    )
+  }
+}
+
+function cloneObj (obj) {
+  return Object.assign({}, obj)
+}
