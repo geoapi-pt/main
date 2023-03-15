fb105e3c626f7fc6b56e8024905c1362de0a8576
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2023-03-15 12:23:10 +0100

    add /meulocal route

---
+++ src/public/js/meulocal-route.js
@@ -0,0 +1,16 @@
+(() => {
+  const meulocalDataDomEl = document.getElementById('meulocal-route-data')
+  const meulocalData = JSON.parse(decodeURIComponent(meulocalDataDomEl.dataset.meulocalroute))
+  window.gpsData = meulocalData
+
+  navigator.geolocation.getCurrentPosition(res => {
+    if (res) {
+      const lat = res.coords.latitude
+      const lon = res.coords.longitude
+
+      window.location.href = `/gps/${lat},${lon}`
+    } else {
+      window.alert('Por favor autorize a obtenção da localização no seu navegador')
+    }
+  })
+})()
