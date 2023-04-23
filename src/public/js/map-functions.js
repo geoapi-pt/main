93cda011e9900d8af176d483d42b89682c6b466c
Waldir Pimenta <waldyrious@gmail.com>
2023-04-23 16:26:58 +0100

    Reorder list of colors for area highligts
    Since the algorithm tries to pick colors from the start of the list, rather than randomly, the order of colors in the list has a marked effect in the resulting distributions of color.
    This proposal changes the color ordering to produce results that I personally find more pleasant.

--- src/public/js/map-functions.js
+++ src/public/js/map-functions.js
@@ -1,6 +1,6 @@
 /* global topojson */
 
-const colors = ['#8dd3c7', '#ffffb3', '#bebada', '#fb8072', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9', '#bc80bd', '#ccebc5', '#ffed6f']
+const colors = ['#8dd3c7', '#ffffb3', '#fb8072', '#bebada', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9', '#bc80bd', '#ccebc5', '#ffed6f']
 
 export function style (feature) {
   return {
