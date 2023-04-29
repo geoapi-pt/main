f517c9fbebd578b7a6db01a868da4cf8c65f03cb
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2023-04-30 00:40:10 +0200

    minify html

--- src/server/middlewares/sendData.js
+++ src/server/middlewares/sendData.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const minify = require('html-minifier').minify
 const appRoot = require('app-root-path')
 const debug = require('debug')('geoapipt:server')
 
@@ -32,6 +33,22 @@ module.exports = ({ configs, shieldsioCounters }) =>
           input: data.input || {},
           data: dataToBeSent, // this is sent to frontend Javascript code
           dataToShowOnHtml: data.dataToShowOnHtml ? adaptObjForHtmlRes(data.dataToShowOnHtml) : {}
+        }, (err, rawHtml) => {
+          if (err && !res.headersSent) {
+            res.status(500).send(`Erro ao processar o template ${template} no caminho ${req.originalUrl}`)
+            console.error(err)
+          } else {
+            if (process.env.NODE_ENV === 'production') {
+              res.send(minify(rawHtml, {
+                collapseWhitespace: true, // collapse white space that contributes to text nodes in a document tree
+                removeComments: true, // strip HTML comments
+                removeOptionalTags: true, // remove optional tags http://perfectionkills.com/experimenting-with-html-minifier/#remove_optional_tags
+                caseSensitive: true // treat attributes in case sensitive manner (useful for custom HTML tags)))
+              }))
+            } else {
+              res.send(rawHtml)
+            }
+          }
         })
       }
     }
