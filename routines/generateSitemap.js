236b56bbacc74e50741da3784473f811c289d590
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2023-05-04 00:02:03 +0200

    improve generation of sitemap

--- routines/generateSitemap.js
+++ routines/generateSitemap.js
@@ -8,7 +8,7 @@ const colors = require('colors/safe')
 // origin=scheme+host+port, ex: http://example.com:8080
 const defaultOrigin = JSON.parse(fs.readFileSync(path.join(appRoot.path, 'configs.json'))).defaultOrigin
 
-const sitemapsDir = path.join(appRoot.path, 'src', 'public')
+const sitemapsDir = path.join(appRoot.path, 'src', 'public', 'src')
 
 const getRegionsAndAdmins = require(path.join(appRoot.path, 'src', 'server', 'services', 'getRegionsAndAdmins.js'))
 const preparePostalCodesCTTMod = require(path.join(__dirname, 'generatePostalCodes', 'prepareCTTfile.js'))
@@ -57,28 +57,51 @@ function createSitemap (cb) {
 
   const links = []
   for (const CP of postalCodes) {
-    links.push({ url: `/cp/${CP}`, changefreq: 'daily', priority: 0.5 })
+    links.push({ url: `/cp/${CP}`, changefreq: 'monthly', priority: 0.5 })
   }
   for (const CP4 of CP4postalCodes) {
-    links.push({ url: `/cp/${CP4}`, changefreq: 'daily', priority: 0.5 })
+    links.push({ url: `/cp/${CP4}`, changefreq: 'monthly', priority: 0.5 })
   }
+
+  links.push({ url: '/municipios', changefreq: 'monthly', priority: 0.5 })
+  links.push({ url: '/municipios/freguesias', changefreq: 'monthly', priority: 0.5 })
 
   // /municipio/{município} and /municipio/{município}/freguesias
   for (const municipality of administrations.listOfMunicipalitiesNames) {
-    links.push({ url: `/municipio/${encodeURIComponent(municipality)}`, changefreq: 'daily', priority: 0.5 })
-    links.push({ url: `/municipio/${encodeURIComponent(municipality)}/freguesias`, changefreq: 'daily', priority: 0.5 })
+    links.push({ url: `/municipio/${encodeStr(municipality)}`, changefreq: 'monthly', priority: 0.5 })
+    links.push({ url: `/municipio/${encodeStr(municipality)}/freguesias`, changefreq: 'monthly', priority: 0.5 })
   }
 
-  // /freguesias/{freguesia}, replace(/ *\([^)]*\) */g, "") removes text between parentheses
-  for (const parish of administrations.listOfParishesNames) {
+  for (const municipality of administrations.listOfMunicipalitiesWithParishes) {
+    for (const parish of municipality.freguesias) {
+      links.push({
+        url: `/municipio/${encodeStr(municipality.nome)}/freguesia/${encodeStr(parish)}`,
+        changefreq: 'monthly',
+        priority: 0.5
+      })
+    }
+  }
+
+  links.push({ url: '/distritos', changefreq: 'monthly', priority: 0.5 })
+  links.push({ url: '/distritos/municipios', changefreq: 'monthly', priority: 0.5 })
+
+  for (const district of administrations.listOfDistricts) {
     links.push({
-      url: `/freguesias/${encodeURIComponent(parish.replace(/ *\([^)]*\) */g, ''))}`,
-      changefreq: 'daily',
+      url: `/distrito/${encodeStr(district)}`,
+      changefreq: 'monthly',
+      priority: 0.5
+    })
+    links.push({
+      url: `/distrito/${encodeStr(district)}/municipios`,
+      changefreq: 'monthly',
+      priority: 0.5
+    })
+    links.push({
+      url: `/distrito/${encodeStr(district)}/freguesias`,
+      changefreq: 'monthly',
       priority: 0.5
     })
   }
-
-  links.push({ url: '/distritos/municipios', changefreq: 'daily', priority: 0.5 })
 
   // writes sitemaps and index out to the destination you provide.
   simpleSitemapAndIndex({
@@ -92,4 +115,8 @@ function createSitemap (cb) {
 
 function removeDuplicatesFromArray (array) {
   return [...new Set(array)]
+}
+
+function encodeStr (str) {
+  return encodeURIComponent(str.toLowerCase())
 }
