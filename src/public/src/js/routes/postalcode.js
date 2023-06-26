d3ef026786d37d3e71bd3c6a2a4e2357318ad76b
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2023-06-26 23:12:08 +0200

    resolver #89

--- src/public/src/js/routes/postalcode.js
+++ src/public/src/js/routes/postalcode.js
@@ -7,49 +7,59 @@ const postcodeDataDomEl = document.getElementById('postcode-data')
 const postcodeData = JSON.parse(decodeURIComponent(postcodeDataDomEl.dataset.postcode))
 window.postcodeData = postcodeData
 
-const map = L.map('map', leafletContextmenu.mapOtions).setView(postcodeData.centro, 11)
-leafletContextmenu.setMap(map)
-mapFunctions.setMap(map)
+console.log('postcodeData:', postcodeData)
 
-if (postcodeData.poligono) {
-  const lats = postcodeData.poligono.map(el => el[0])
-  const lons = postcodeData.poligono.map(el => el[1])
-  map.fitBounds([
-    [Math.min(...lats), Math.min(...lons)],
-    [Math.max(...lats), Math.max(...lons)]
-  ], { padding: [30, 30] })
-} else if (postcodeData.pontos && postcodeData.pontos.length > 1) {
-  const lats = postcodeData.pontos.map(el => el.coordenadas[0])
-  const lons = postcodeData.pontos.map(el => el.coordenadas[1])
-  map.fitBounds([
-    [Math.min(...lats), Math.min(...lons)],
-    [Math.max(...lats), Math.max(...lons)]
-  ], { padding: [30, 30] })
-}
+if (
+  Array.isArray(postcodeData.centro) && postcodeData.centro.length === 2 &&
+  Array.isArray(postcodeData.pontos) && postcodeData.pontos.length
+) {
+  const map = L.map('map', leafletContextmenu.mapOtions).setView(postcodeData.centro, 11)
+  leafletContextmenu.setMap(map)
+  mapFunctions.setMap(map)
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '© OpenStreetMap'
-}).addTo(map);
-
-['centroide', 'centroDeMassa', 'centro'].forEach(el => {
-  if (postcodeData[el]) {
-    const elMap = L.marker(postcodeData[el]).addTo(map)
-    elMap.bindPopup(el)
+  if (postcodeData.poligono) {
+    const lats = postcodeData.poligono.map(el => el[0])
+    const lons = postcodeData.poligono.map(el => el[1])
+    map.fitBounds([
+      [Math.min(...lats), Math.min(...lons)],
+      [Math.max(...lats), Math.max(...lons)]
+    ], { padding: [30, 30] })
+  } else if (postcodeData.pontos && postcodeData.pontos.length > 1) {
+    const lats = postcodeData.pontos.map(el => el.coordenadas[0])
+    const lons = postcodeData.pontos.map(el => el.coordenadas[1])
+    map.fitBounds([
+      [Math.min(...lats), Math.min(...lons)],
+      [Math.max(...lats), Math.max(...lons)]
+    ], { padding: [30, 30] })
   }
-})
 
-if (postcodeData.poligono) {
-  L.polygon(postcodeData.poligono).addTo(map)
-}
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+  }).addTo(map);
 
-postcodeData.pontos
-  .forEach(el => {
-    const ponto = L.circle(el.coordenadas, {
-      color: 'red',
-      fillColor: '#f03',
-      fillOpacity: 0.5,
-      radius: 2
-    }).addTo(map)
-    ponto.bindPopup(`${el.rua}${el.casa ? ', ' + el.casa : ''}`)
+  ['centroide', 'centroDeMassa', 'centro'].forEach(el => {
+    if (postcodeData[el]) {
+      const elMap = L.marker(postcodeData[el]).addTo(map)
+      elMap.bindPopup(el)
+    }
   })
+
+  if (postcodeData.poligono) {
+    L.polygon(postcodeData.poligono).addTo(map)
+  }
+
+  postcodeData.pontos
+    .forEach(el => {
+      const ponto = L.circle(el.coordenadas, {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 2
+      }).addTo(map)
+      ponto.bindPopup(`${el.rua}${el.casa ? ', ' + el.casa : ''}`)
+    })
+} else {
+  const mapEl = document.getElementsByClassName('map-div')
+  mapEl[0].style.display = 'none'
+}
