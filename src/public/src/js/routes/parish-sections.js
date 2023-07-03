d1d772e2bcf1550c43c83e14823a2540e2d447bc
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2023-07-03 22:05:05 +0200

    add route

---
+++ src/public/src/js/routes/parish-sections.js
@@ -0,0 +1,86 @@
+/* global L */
+
+import * as leafletContextmenu from '../map/leafletContextmenu.js'
+import { mobileCheck } from '../functions.js'
+import * as mapFunctions from '../map/map-functions.js'
+
+const parishSectionsDataDomEl = document.getElementById('parish-sections-route-data')
+const parishSectionsData = JSON.parse(decodeURIComponent(parishSectionsDataDomEl.dataset.parishsectionsroute))
+window.parishSectionsData = parishSectionsData
+
+console.log('parishSectionsData:', parishSectionsData)
+
+const sectionGeoJsonFeatureCollection = mapFunctions.getGeojsonFeatureCollection(parishSectionsData.geojson.features)
+
+const map = L.map('map', leafletContextmenu.mapOtions)
+leafletContextmenu.setMap(map)
+mapFunctions.setMap(map)
+
+const bbox = parishSectionsData.geojson.bbox
+const corner1 = L.latLng(bbox[1], bbox[0])
+const corner2 = L.latLng(bbox[3], bbox[2])
+const bounds = L.latLngBounds(corner1, corner2)
+map.fitBounds(bounds)
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map)
+
+// control that shows state info on hover
+const info = L.control()
+
+info.onAdd = function (map) {
+  this._div = L.DomUtil.create('div', 'info')
+  this.update()
+  return this._div
+}
+
+info.update = function (props) {
+  let contents = '<h4>Secções estatísticas</h4>'
+  if (props && props.SECNUM21) {
+    contents += `Secção <b>${props.SECNUM21}</b>`
+  } else {
+    if (mobileCheck()) {
+      contents += 'Toque numa secção ou faça-lhe duplo toque.<br>Pressione longamente num ponto do mapa para mais opções.'
+    } else {
+      contents += '<b>Mova o rato sobre uma secção ou faça-lhe (duplo)clique.<br>Clique no botão direito do rato num ponto do mapa para mais opções.</b>'
+    }
+  }
+
+  this._div.innerHTML = contents
+}
+
+info.addTo(map)
+
+const geojsonLayer = L.geoJson(sectionGeoJsonFeatureCollection, {
+  style: mapFunctions.style,
+  onEachFeature
+}).addTo(map)
+
+function onEachFeature (feature, layer) {
+  layer.on({
+    mouseover: mapFunctions.getHighlightFeature(info),
+    mouseout: resetHighlight,
+    click: (e) => mapFunctions.getZoomToFeature(e),
+    dblclick: forwardToPage
+  })
+}
+
+function resetHighlight (e) {
+  geojsonLayer.resetStyle(e.target)
+  info.update()
+}
+
+function forwardToPage (e) {
+  map.clicked = 0
+  const municipality = parishSectionsData.municipio.nome
+  const parish = parishSectionsData.freguesia.nome
+  const section = e.target.feature.properties.SECNUM21.padStart(3, '0')
+  if ([municipality, parish, section].every(e => typeof e === 'string')) {
+    window.location.href =
+      `/municipio/${encodeURIComponent(municipality.toLowerCase())}/freguesia/${encodeURIComponent(parish.toLowerCase())}/sec/${section}`
+  }
+}
+
+map.attributionControl.addAttribution('Carta Administrativa Oficial de Portugal <a href="https://www.dgterritorio.gov.pt/">Direção Geral do Território</a>')
---
+++ src/server/routes/municipiosMunicipalityFreguesiasParishSeccoes.js
@@ -0,0 +1,115 @@
+const fs = require('fs')
+const path = require('path')
+const appRoot = require('app-root-path')
+const turf = require('@turf/turf')
+const debug = require('debug')('geoapipt:server')
+
+const { normalizeName } = require(path.join(__dirname, '..', 'utils', 'commonFunctions.js'))
+const isResponseJson = require(path.join(appRoot.path, 'src', 'server', 'utils', 'isResponseJson.js'))
+
+const geojsonDir = path.join(appRoot.path, 'res', 'geojson')
+
+module.exports = {
+  fn: routeFn,
+  route: '/municipios?/:municipality?/freguesias?/:parish?/sec(c|ç|%C3%A7)o(e|ẽ|%E1%BA%BD)s'
+}
+
+async function routeFn (req, res, next, { administrations, regions }) {
+  debug(req.path, req.query, req.headers)
+
+  if (!req.params.municipality || !req.params.parish) {
+    next()
+    return
+  }
+
+  const municipality = req.params.municipality
+  const parish = req.params.parish
+
+  let parishResults = [...administrations.parishesDetails]
+
+  const parishToFind = normalizeName(parish)
+  const municipalityToFind = normalizeName(municipality)
+
+  parishResults = parishResults.filter(parish => {
+    const name0 = normalizeName(parish.nome)
+    const name1 = normalizeName(parish.nomecompleto)
+    const name2 = normalizeName(parish.nomecompleto2)
+    const name3 = normalizeName(parish.nomecompleto3)
+    return (
+      (
+        parishToFind === name0 ||
+        parishToFind === name1 ||
+        parishToFind === name2 ||
+        parishToFind === name3
+      ) &&
+      municipalityToFind === normalizeName(parish.municipio)
+    )
+  })
+
+  parishResults = JSON.parse(JSON.stringify(parishResults)) // deep clone
+
+  if (parishResults.length === 1) {
+    const parish = parishResults[0]
+
+    const parishCode = parish.codigoine.padStart(6, '0')
+    const municipalityCode = parishCode.slice(0, 4)
+
+    const municipality = administrations.municipalitiesDetails
+      .find(e => parseInt(e.codigoine) === parseInt(municipalityCode))
+
+    // data to be sent to client
+    const sectionsObj = {
+      freguesia: parish,
+      municipio: municipality
+    }
+
+    const geojsonFile = path.join(geojsonDir, 'seccoes', '2021', `${municipalityCode}.json`)
+
+    if (!fs.existsSync(geojsonFile)) {
+      res.status(404).sendData({ error: `Ficheiro Geojson ${path.relative(appRoot.path, geojsonFile)} não encontrado!` })
+      return
+    }
+
+    let sectionsGeojson, filteredSectionsGeojson
+    // eslint-disable-next-line prefer-const
+    sectionsGeojson = filteredSectionsGeojson = JSON.parse(fs.readFileSync(geojsonFile, 'utf-8'))
+
+    filteredSectionsGeojson.features = sectionsGeojson.features
+      .filter(el => el.properties.DTMNFR21.padStart(6, '0') === parishCode)
+
+    if (filteredSectionsGeojson.features.length) {
+      sectionsObj.geojson = filteredSectionsGeojson
+      sectionsObj.geojson.bbox = turf.bbox(filteredSectionsGeojson)
+    } else {
+      res.status(404).sendData({
+        error: `Secções estatísticas não encontradas para a freguesia ${parish.nome} do município ${municipality.nome}!`
+      })
+      return
+    }
+
+    if (isResponseJson(req)) {
+      res.status(200).sendData({ data: sectionsObj })
+    } else {
+      // html/text response
+      const dataToShowOnHtml = {}
+
+      dataToShowOnHtml.freguesia = parish.nome
+      dataToShowOnHtml.municipio = municipality.nome
+      dataToShowOnHtml.distrito = municipality.distrito
+
+      res.status(200).sendData({
+        data: sectionsObj,
+        input: {
+          Freguesia:
+            `<a href="/municipio/${encodeURIComponent(parish.municipio.toLowerCase())}/freguesia/${encodeURIComponent(parish.nomecompleto.toLowerCase())}">${parish.nomecompleto}</a> ` +
+            `(<a href="/municipio/${parish.municipio.toLowerCase()}">${parish.municipio}</a>)`
+        },
+        dataToShowOnHtml: dataToShowOnHtml,
+        pageTitle: `Secções estatísticas da Freguesia ${parish.nomecompleto} (${parish.municipio})`,
+        template: 'routes/parishSections'
+      })
+    }
+  } else {
+    res.status(404).sendData({ error: `Secções não encontradas para a freguesia ${parish.nome} do município ${municipality.nome}!` })
+  }
+}
