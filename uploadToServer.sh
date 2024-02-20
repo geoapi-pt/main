d961cfd56f11679f60cba5f8deb01e62deb86fb6
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2024-02-20 20:37:34 +0100

    update upload to server

--- uploadToServer.sh
+++ uploadToServer.sh
@@ -1,4 +1,5 @@
 # NOTE: This script is only to be used by the owner of the project
 cd "$(dirname "$0")"
+npm ci
 rsync --verbose --compress --recursive --links --times --delete --exclude '*.sh' --exclude '.git*' --exclude 'counters.json' . jfolpf@contabo.joaopimentel.com:/var/www/geoapipt/
 ssh jfolpf@contabo.joaopimentel.com -- '/usr/local/bin/pm2 reload geoapi.pt'
