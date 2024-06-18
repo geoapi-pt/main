55a387cfdfe0a97e9843262ff742640f0aeb678b
João Pimentel Ferreira <joao.pimentel.ferreira@gmail.com>
2024-06-18 12:32:38 +0200

    Update common.js

--- src/public/src/js/charts/common.js
+++ src/public/src/js/charts/common.js
@@ -1,21 +1,42 @@
 // see file censosChartsMaping.json
 export function getData (obj, censos) {
   const data = Object.values(obj).map(el => {
-    if (Array.isArray(el)) {
-      for (const el_ of el) {
-        if (!el_.includes('+')) {
-          if (censos[el_]) return censos[el_]
-        } else {
-          return el_
-            .split('+')
-            .map(e => e.trim())
-            .reduce((accumulator, a) => accumulator + (censos[a] || 0), 0)
-        }
+    if (Array.isArray(el)) {    
+      for (const indEl of el) {
+        const res = processExpression(indEl, censos)
+        if(res) return res
       }
       return 0
     } else {
-      return censos[el]
+      return processExpression(el, censos)
     }
   })
+
   return data
+}
+
+function processExpression (expr_, censos) {
+  const expr = expr_.trim()
+  // expression does not have math symbols +, * or /
+  if (!/\+|\*|\//.test(expr)) {
+    if (censos[expr]) { 
+      return censos[expr]
+    } else {
+      return 0
+    }
+  } else {
+    // it has math symbols +, * or /
+    let expr2 = expr
+    expr
+      .split(/\+|\*|\//)
+      .map(el => el.trim())
+      .forEach(el => {
+        if(censos[el]) {
+          expr2.replace(el, censos[el])
+        } else {
+          expr2.replace(el, 0)
+        }
+      })
+    return eval(expr2)
+  }
 }
