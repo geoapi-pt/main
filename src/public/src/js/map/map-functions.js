b854f882797dff762a0cc6fda9693008bc3093df
jfoclpf <joao.pimentel.ferreira@gmail.com>
2024-08-04 22:47:42 +0100

    solve #121

--- src/public/src/js/map/map-functions.js
+++ src/public/src/js/map/map-functions.js
@@ -9,7 +9,10 @@ delete L.Icon.Default.prototype._getIconUrl
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
   iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  iconSize: [25, 41],
+  iconAnchor: [12.5, 41],
+  shadowSize: [41, 41]
 })
 
 export function setMap (_map) {
